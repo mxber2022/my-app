d15daf1550ede1cf04cf98bbe25dd28565d5088b
mxber2022 <mxber2022@gmail.com>
2025-04-05 22:02:27 +0800

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,26 +9,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-export default [
-  // Use a compatible parser for TS/JSX
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        ecmaVersion: 2022,
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true,
-        },
-        project: "./tsconfig.json",
-      },
-    },
-  },
-
-  ...compat.extends(
-    "next",
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended"
-  ),
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+
+export default eslintConfig;
