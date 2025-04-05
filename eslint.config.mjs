15f163f9680631b89f3de188ee02719188f7746f
mxber2022 <mxber2022@gmail.com>
2025-04-05 21:45:25 +0800

    s

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,34 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    // Ignores ESLint errors during builds
-    ignoreDuringBuilds: true,
-  },
-  // Other config options if needed
-};
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-module.exports = nextConfig;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  // Use a compatible parser for TS/JSX
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: "./tsconfig.json",
+      },
+    },
+  },
+
+  ...compat.extends(
+    "next",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended"
+  ),
+];
