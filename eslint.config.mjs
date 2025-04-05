5e21b5c50842f20b715fd6c7cd2238edf3703efd
mxber2022 <mxber2022@gmail.com>
2025-04-05 21:38:13 +0800

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,11 @@
-export default [];
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    // Ignores ESLint errors during builds
+    ignoreDuringBuilds: true,
+  },
+  // Other config options if needed
+};
+
+module.exports = nextConfig;
